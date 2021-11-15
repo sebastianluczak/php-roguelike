64c6fb4ebdbce62e17049148d3c4cb7003f37fa8
Sebastian Łuczak <sebastian.m.luczak@gmail.com>
2021-11-15 15:55:47 +0100

    Fixes leaderboards persistence bug

---
+++ src/Command/GameTestCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\GameService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GameTestCommand extends Command
+{
+    protected static $defaultName = 'game:test';
+    protected GameService $gameService;
+
+    public function __construct(GameService $gameService, string $name = null)
+    {
+        parent::__construct($name);
+        $this->gameService = $gameService;
+    }
+
+    protected function configure(): void
+    {
+        $this->setHelp('Test of Game');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        var_dump($this->gameService->getLeaderboardService()->getBestScores());
+        return Command::SUCCESS;
+    }
+
+    private function clearScreen()
+    {
+        echo chr(27).chr(91).'H'.chr(27).chr(91).'J';   //^[H^[J
+    }
+}
